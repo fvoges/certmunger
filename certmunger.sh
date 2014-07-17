2b0e27e5efb91ccbbce73bccc6b18530c4a7f249
Federico Voges <federico@puppetlabs.com>
2014-07-17 12:20:43 +0200

    renamed the output files

--- certmunger.sh
+++ certmunger.sh
@@ -10,8 +10,8 @@ fi
 CERTNAME="$(dirname $1)/$(basename $1 .pfx)"
 CERTPASS="$(basename $1 .pfx)"
 
-openssl pkcs12 -in ${CERTNAME}.pfx -passin pass:${CERTPASS} -out ${CERTNAME}.key.pem -nocerts -nodes
-openssl pkcs12 -in ${CERTNAME}.pfx -passin pass:${CERTPASS} -out ${CERTNAME}.crt.pem -clcerts -nodes -nokeys
-openssl pkcs12 -in ${CERTNAME}.pfx -passin pass:${CERTPASS} -out ${CERTNAME}.ca.pem -cacerts -nodes -nokeys
-openssl rsa -in ${CERTNAME}.key.pem -out ${CERTNAME}.pubkey.pem -pubout
+openssl pkcs12 -in ${CERTNAME}.pfx -passin pass:${CERTPASS} -out ${CERTNAME}.private_key.pem -nocerts -nodes
+openssl pkcs12 -in ${CERTNAME}.pfx -passin pass:${CERTPASS} -out ${CERTNAME}.cert.pem -clcerts -nodes -nokeys
+openssl pkcs12 -in ${CERTNAME}.pfx -passin pass:${CERTPASS} -out ${CERTNAME}.ca_cert.pem -cacerts -nodes -nokeys
+openssl rsa -in ${CERTNAME}.private_key.pem -out ${CERTNAME}.public_key.pem -pubout
 
